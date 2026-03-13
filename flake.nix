462a5a19adb5f71d4171ff130227be12a690bbdc
mzkrl <apasihsh@gmail.com>
2026-03-14 00:59:53 +0700

    f

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description: "NixOS Configuration with Caelestia Shell and Home Manager";
+  description = "NixOS Configuration with Caelestia Shell and Home Manager";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
