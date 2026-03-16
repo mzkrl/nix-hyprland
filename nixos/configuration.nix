96eec3f1b2ceee1ca3d0dba4345dd9c64ad5d0a9
mzkrl <apasihsh@gmail.com>
2026-03-16 10:03:00 +0700

    leh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -157,7 +157,7 @@ environment.systemPackages = with pkgs; [
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
         user = "juang";
       };
     };
